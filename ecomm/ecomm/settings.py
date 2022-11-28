a5d4fc2584ff600313e5c8402cab8bfb0d53adea
SohailBadeghar <86312824+SohailBadeghar@users.noreply.github.com>
2022-11-28 12:14:36 +0530

    Update settings.py
    Changes in Database

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -111,15 +111,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecomm.wsgi.application'
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'Eshop_Db',
+#        'USER': 'postgres',
+#        'PASSWORD':'root',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'Eshop_Db',
-       'USER': 'postgres',
-       'PASSWORD':'root',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
